e949bcc7ffab7a28720004ab6fd5d5735c366cdc
AhmedYasirJwd <146742669+AhmedYasirJwd@users.noreply.github.com>
2024-07-14 13:10:41 +0500

    Add files via upload
    Uploaded the python file

---
+++ lab_1.py
@@ -0,0 +1,106 @@
+import csv
+
+def read_tasks(filename='tasks.csv'):
+    tasks = []
+    try:
+        with open(filename, mode='r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if 'Task' in row:
+                    tasks.append({'Task': row['Task']})
+    except FileNotFoundError:
+        pass
+    return tasks
+
+
+
+
+
+
+def write_tasks(tasks, filename='tasks.csv'):
+    with open(filename, mode='w', newline='') as file:
+        fieldnames = ['Task']
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(tasks)
+
+
+
+
+
+
+def add_task(task, filename='tasks.csv'):
+    tasks = read_tasks(filename)
+    tasks.append({'Task': task})
+    write_tasks(tasks, filename)
+
+
+
+
+def remove_task(task_index, filename='tasks.csv'):
+    tasks = read_tasks(filename)
+    if 0 <= task_index < len(tasks):
+        tasks.pop(task_index)
+        write_tasks(tasks, filename)
+        return True
+    else:
+        return False
+
+
+
+
+
+def view_tasks(filename='tasks.csv'):
+    tasks = read_tasks(filename)
+    if not tasks:
+        print("\t\t\t\tNo tasks found.")
+    else:
+        for index, task in enumerate(tasks):
+            print(f"{index + 1}. {task['Task']}")
+
+
+
+
+
+def main():
+    
+    while True:
+        print("\n\t\t\t\t\t\t\t\t\033[1mPython To-do List\033[0m")
+        view_tasks()
+        
+        print("\n\t\t\t\tSelect any option:")
+        print("\t\t\t\t1. Add Task")
+        print("\t\t\t\t2. Remove Task")
+        print("\t\t\t\t3. Exit")
+        choice = input("Choose an option: ")
+        
+
+
+        if choice == '1':
+            task = input("Enter the task: ")
+            add_task(task)
+            print("Task added successfully.")
+
+
+
+        elif choice == '2':
+            try:
+                task_index = int(input("Enter the task number to remove: ")) - 1
+                if remove_task(task_index):
+                    print("Task removed successfully.")
+                else:
+                    print("Invalid task number.")
+            except ValueError:
+                print("Please enter a valid number.")
+
+
+
+        elif choice == '3':
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+
+
+if __name__ == "__main__":
+    main()
